9189928cf63fcdf1d22830d47e19983edaab4d19
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-05-30 00:43:00 -0500

    initial setup of grades exercise

---
+++ grades.js
@@ -0,0 +1,8 @@
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+// Use `console.log()` to output the following criteria:
+
+// 1. How many of each grade?
+// 1. What is the lowest grade?
+// 1. What is the highest grade?
+
